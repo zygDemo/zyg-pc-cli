ec54db5f6c4d411b89d935490bede0974646987d
zyg <1519881356@qq.com>
2024-07-17 16:37:21 +0800

    代码提交

---
+++ .eslintrc.cjs
@@ -0,0 +1,12 @@
+/* eslint-env node */
+module.exports = {
+  root: true,
+  "vue/valid-template-root": "off",
+  'extends': [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  }
+}
